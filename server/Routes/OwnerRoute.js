c65116fa8cbad506380bdab4feba366305a2c862
Anees <aneesaboo123@gmail.com>
2025-09-05 11:32:29 +0530

    added:routes and backend for the availablity,delete-car,car-list display and add the model for booking as well

--- server/Routes/OwnerRoute.js
+++ server/Routes/OwnerRoute.js
@@ -1,13 +1,25 @@
 import express from "express";
 import { protect } from "../middleware/Auth.js";
-import { addCar, changeRoleToOwner } from "../controllers/ownerController.js";
+import {
+  addCar,
+  changeRoleToOwner,
+  getOwnerCars,
+  ownerDeleteCar,
+  toggleCarAvailability,
+} from "../controllers/ownerController.js";
 import upload from "../middleware/multer.js";
 
 const ownerRoute = express.Router();
-
+// protect is middleware
 // api for changing the owner
 ownerRoute.post("/change-role", protect, changeRoleToOwner);
 // api to upload a file and add the car
 ownerRoute.post("/add-car", upload.single("image"), protect, addCar);
+// api to get owner cars
+ownerRoute.get("/cars", protect, getOwnerCars);
+// api to ggle car
+ownerRoute.post("/toggle-car", protect, toggleCarAvailability);
+// api to delete car
+ownerRoute.post("/delete-car", protect, ownerDeleteCar);
 
 export default ownerRoute;
--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -36,7 +36,7 @@ export const addCar = async (req, res) => {
       transformation: [
         { width: "1280" },
         { quality: "auto" }, //automatically compress
-        { format: "webp" }//convert to modern format
+        { format: "webp" }, //convert to modern format
       ],
     });
 
@@ -44,6 +44,85 @@ export const addCar = async (req, res) => {
     // adding this image to database
     await Car.create({ ...car, owner: _id, image });
     res.json({ success: true, message: "Car Added" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// api to list owner cars
+export const getOwnerCars = async (req, res) => {
+  try {
+    // id of whoever logged in based on the middleware
+    const { _id } = req.user;
+    // based on the owner id on car finding cars
+    const cars = await Car.find({ owner: _id });
+    res.json({ success: true, cars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// for changing car available  or not
+export const toggleCarAvailability = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.findById(carId);
+
+    // check the car belong to user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorised" });
+    }
+    // update only the matchIds car and update only availabilityy
+    car.isAvailable = !car.isAvailable;
+    await car.save();
+
+    res.json({
+      success: true,
+      message: "successfullt update the availability",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// abiliy to delete the owner car by the owner
+export const ownerDeleteCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.findById(carId);
+
+    // check the car belong to user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "unAuthorised" });
+    }
+    car.owner = null;
+    car.isAvailable = false;
+
+    await car.save();
+
+    res.json({ success: true, message: "Car Removed" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Api to get dashboard data
+export const getDashboardData = async (req, res) => {
+  try {
+    const { _id, role } = req.user;
+
+    // not owner
+    if (role !== "owner") {
+      return res.json({ success: false, message: "Unauthorised" });
+    }
+
+    const cars = await Car.find({ owner: _id });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
